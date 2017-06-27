1b9f5ef91929f2486e0984bdf24152ed2fb957f4
719Ben <ben.williams@colorado.edu>
2017-06-27 02:04:22 -0700

    Cleaned up code

--- rewriter.js
+++ rewriter.js
@@ -17,12 +17,11 @@ const cleanupReadme = (newContent, sha, filepath, branch, owner, repo) => {
     repo
   }, function (err, res) {
     if (err) {
-      console.log(err)
+      console.log(`There was an error forking on ${owner}/${repo}`)
+      return
     }
     let forkOwner = res.data.owner.login
     let forkRepo = res.data.name
-
-    console.log(forkOwner, forkRepo)
 
     github.repos.updateFile({
       owner: forkOwner,
@@ -33,22 +32,25 @@ const cleanupReadme = (newContent, sha, filepath, branch, owner, repo) => {
       sha: sha
     }, (err, res) => {
       if (err) {
-        console.log(err)
+        console.log(`There was an error updating a file on ${owner}/${repo}`)
+        return
       }
 
-      console.log(res)
-      // console.log(`${forkOwner}:${forkRepo}`)
+      let requestBody = 'Hello,\nI am a bot that helps clean up GitHub Repo README header tags. A while ago GitHub changed their markdown render engine and all markdown # tags without a space now break. Let me know if you have any questions!'
 
       github.pullRequests.create({
         owner: owner,
         repo: repo,
         title: 'Cleaned up heading tags in readme',
+        body: requestBody,
         base: branch,
         head: `${forkOwner}:${branch}`
       }, (err, res) => {
         if (err) {
-          console.log(err)
+          console.log(`There was an error creating a pull request on ${owner}/${repo}`)
+          return
         }
+        console.log(`Created pull request on ${owner}/${repo}`)
       })
     })
   })
@@ -65,28 +67,38 @@ const checkRepo = (owner, repo) => {
     repo
   }, (err, res) => {
     if (err) {
-      console.log(err)
+      console.log(`No readme found for ${owner}/${repo}`)
+      return
     }
 
     let filePath = res.data.path
-
-    let readmeText = Buffer.from(res.data.content, 'base64').toString()
     let readmeSHA = res.data.sha
 
-    // console.log(readmeText)
+    let readmeText = Buffer.from(res.data.content, 'base64').toString()
     let editedReameText = addSpaces(readmeText)
 
-    // console.log(editedReameText)
     let branch = res.data.html_url.split('blob/')[1].split('/')[0]
 
     if (editedReameText !== readmeText) {
       cleanupReadme(editedReameText, readmeSHA, filePath, branch, owner, repo)
-      console.log('Found something different!')
+      console.log(`Found something different on '${owner}/${repo}'`)
     } else {
-      // console.log('well that was anticlimatic... nothing changed')
+      console.log(`well that was anticlimatic... nothing changed on '${owner}/${repo}'`)
     }
   })
 }
 
-checkRepo('719ben', 'liar')
-checkRepo('719ben', 'Baudrate.py')
+let username = 'agent-reed'
+
+github.repos.getForUser({
+  username
+}, (err, res) => {
+  if (err) {
+    console.log(`there was an error getting the repos for ${username}`)
+  }
+
+  for (let repository of res.data) {
+    console.log(`looking at ${repository.name}`)
+    checkRepo(repository.owner.login, repository.name)
+  }
+})
